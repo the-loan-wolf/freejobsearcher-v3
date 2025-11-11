eb1a222cc968691dd4915845fdaad4a793902f48
rishav <rishavb518@gmail.com>
2025-11-11 12:00:21 +0530

    feat: making a profile favorite

--- components/app-components/candidate-card.tsx
+++ components/app-components/candidate-card.tsx
@@ -8,6 +8,9 @@ import {
   AvatarFallback,
 } from "@/components/app-components/ui/avatar";
 import Link from "next/link";
+import { User } from "firebase/auth";
+import { toast } from "sonner";
+import { useState } from "react";
 
 interface Candidate {
   id: string;
@@ -22,6 +25,7 @@ interface Candidate {
 
 interface CandidateCardProps {
   candidate: Candidate;
+  user: User | null;
 }
 
 export function CandidateGridSkeleton() {
@@ -75,7 +79,17 @@ export function CandidateGridSkeleton() {
   );
 }
 
-export function CandidateCard({ candidate }: CandidateCardProps) {
+export function CandidateCard({ candidate, user }: CandidateCardProps) {
+
+  // --- STATES --- //
+  const [condition, setCondition] = useState("none");
+
+  // --- HANDLERS --- //
+  const favoriteButtonHandler = () => {
+    if (!user) toast.warning("Need to Sign In first");
+    setCondition((prev) => (prev === "none" ? "yellow" : "none"));
+  };
+
   return (
     <Card className="group transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl hover:shadow-primary/10 border-primary/10 hover:border-primary/30">
       <CardContent className="p-6">
@@ -152,8 +166,22 @@ export function CandidateCard({ candidate }: CandidateCardProps) {
                 variant="outline"
                 size="sm"
                 className="border-primary/20 hover:border-primary/40 hover:bg-primary/10 hover:text-primary bg-transparent"
+                onClick={favoriteButtonHandler}
               >
-                <Star className="h-4 w-4" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill={condition}
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-star-icon lucide-star h-4 w-4"
+                >
+                  <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
+                </svg>
               </Button>
             </div>
           </div>
