8aa4e202ecef496b00c81cf2bbd7927e112e7134
rishav <rishavb518@gmail.com>
2025-11-11 13:15:54 +0530

    added working favorite button on the profile/[id] route

--- app/app/profile/[id]/page.tsx
+++ app/app/profile/[id]/page.tsx
@@ -5,7 +5,6 @@ import { notFound } from "next/navigation";
 import {
   MapPin,
   Clock,
-  Star,
   Mail,
   Phone,
   Calendar,
@@ -32,6 +31,7 @@ import { useQuery } from "@tanstack/react-query";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 import { app } from "@/lib/firebaseLib";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { toast } from "sonner";
 
 const auth = getAuth(app);
 const db = getFirestore(app);
@@ -113,7 +113,19 @@ export default function ProfilePage({ params }: ProfilePageProps) {
   const { id } = use(params);
   const candidateId = id;
   if (!candidateId) notFound();
+
+  // --- STATES --- //
   const [user, setUser] = useState<User | null>(null);
+  const [condition, setCondition] = useState("none");
+
+  // --- HANDLERS --- //
+  const favoriteButtonHandler = () => {
+    if (!user) {
+      toast.warning("Need to Sign In first");
+    } else {
+      setCondition((prev) => (prev === "none" ? "yellow" : "none"));
+    }
+  };
 
   // Listen for Firebase auth changes
   useEffect(() => {
@@ -211,8 +223,22 @@ export default function ProfilePage({ params }: ProfilePageProps) {
                       <Button
                         variant="outline"
                         className="w-full bg-transparent"
+                        onClick={favoriteButtonHandler}
                       >
-                        <Star className="h-4 w-4 mr-2" />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width={24}
+                          height={24}
+                          viewBox="0 0 24 24"
+                          fill={condition}
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="lucide lucide-star-icon lucide-star h-4 w-4"
+                        >
+                          <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
+                        </svg>
                         Save Profile
                       </Button>
                     </div>
